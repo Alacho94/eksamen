8b81a54522295d17df4c67ea57cf7bf19dbd96b0
OlavOlsm <OlavOlsm@gmail.com>
2015-03-16 14:03:49 +0100

    Endret sql spørringer til sanitize parametere med PDO for å hindre sql injection, og la til flere kommentarer

--- nettside/bookRom.php
+++ nettside/bookRom.php
@@ -8,19 +8,21 @@
     	$dato = $_POST['dato'];
     	$romNr = $_POST['romnr'];
         
+        // Sjekk om bruker har skrevet et brukernavn
     	if(isset($_POST['brukernavn']) AND !empty($_POST['brukernavn'])) {
     		$brukernavn = $_POST['brukernavn'];
     	} else {
     		error("Du må skrive inn et brukernavn");
     	}
         
+        // Sjekk om bruker har skrevet et passord
     	if (isset($_POST['passord']) AND !empty($_POST['passord'])) {
     		$passord = $_POST['passord'];
-    		$passord = md5($passord);	// krypter passordet med MD5
     	} else {
     		error("Du må skrive inn et passord");
     	}
         
+        // Sjekk om bruker har valgt minimum en time å holde av
         if (isset($_POST['timer']) AND !empty($_POST['timer'])) {
     		$timer = $_POST['timer'];
     	} else {
@@ -28,31 +30,39 @@
     	}
         
     	// Hent bruker ID og tilhørende passord fra database
-	    $sql = $database->prepare("
-	    	SELECT `brukerID`, `passord` FROM `brukere` WHERE `brukernavn` = '" . $brukernavn . "'");
-		$sql -> execute();
+		$sql = $database->prepare("SELECT `brukerID`, `passord` FROM `brukere` WHERE `brukernavn` = :brukernavn");
+		$sql->bindParam(':brukernavn', $brukernavn, PDO::PARAM_INT);	// Bruk sanitize for å forhindre SQL-injection
+		$sql->execute();
 		$user = $sql -> fetch(PDO::FETCH_OBJ);
 		$userID = $user -> brukerID;
 		$userPW = $user -> passord;
 		$userPW = md5($passord);	// krypter passord med MD5
         
-		// Sjekk om valgt brukernavn eksisterer og passord er riktig før fortsettelse av bestilling
-		if (isset($userID) AND isset($passord) AND $userPW = $passord) {
-			// Lagre bestilling av rom i databasen på rom, bruker, dato
-		    $sql = $database->prepare("
-		        INSERT INTO " . $db . ".`booking` (`brukerID`, `romNr`, `dato`) VALUES (" . $userID . "," . ($romNr) . ", '" . $dato . "') 
-		    ");
-		    $sql->execute();
-		    // Hent booking id for bestillingen av rom
-		    $bookingID = $database->lastInsertId('bookingID');
-		    // Lagre bestilling av timene brukeren har valgt
-			foreach ($timer as $timeID) {
-				$sql = $database->prepare("
-		        	INSERT INTO " . $db . ".`bookingTimer` (`bookingID`, `timeID`) VALUES ( " . $bookingID . "," . $timeID . ") 
-		   		");
-		      $sql->execute();
+        if (!isset($error)) {
+			// Sjekk om valgt brukernavn eksisterer og passord er riktig før fortsettelse av bestilling
+			if (isset($userID) AND isset($passord) AND $userPW = $passord) {
+
+				// Lagre bestilling av rom i databasen på rom, bruker, dato
+				$sql = $database->prepare("INSERT INTO `booking` (`brukerID`, `romNr`, `dato`) VALUES (:userID, :romNr, :dato)");
+				$sql->bindParam(':userID', $userID, PDO::PARAM_STR);
+				$sql->bindParam(':romNr', $romNr, PDO::PARAM_INT);
+				$sql->bindParam(':dato', $dato, PDO::PARAM_STR);
+				$sql->execute();
+
+			    // Hent booking id for bestillingen av rom
+			    $bookingID = $database->lastInsertId('bookingID');
+
+				// Lagre bestilling av timene brukeren har valgt
+				foreach ($timer as $timeID) {
+				    $sql = $database->prepare("INSERT INTO `bookingTimer` (`bookingID`, `timeID`) VALUES (:bookingID, :timeID)");
+					$sql->bindParam(':bookingID', $bookingID, PDO::PARAM_INT);
+					$sql->bindParam(':timeID', $timeID, PDO::PARAM_INT);
+					$sql->execute();
+				}
+
+				// Bestilling er utført.
+			    header("Location: index.php?suksess");
 			}
-		    header("Location: index.php?suksess");
 		}
         
 		// avbryt bestilling dersom brukernavn ikke eksisterer eller passord er feil
@@ -66,5 +76,4 @@
 		header("Location: index.php?error=$error");
 		exit;
 	}
-?>
 ?>
