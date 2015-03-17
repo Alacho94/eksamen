ad77add583177625216de53ef5faf2b5c3b3ef3f
OlavOlsm <OlavOlsm@gmail.com>
2015-03-17 10:57:18 +0100

    Kontroller at dato er minst i dag og maks om 30 dager

--- nettside/bookRom.php
+++ nettside/bookRom.php
@@ -4,30 +4,37 @@
     function bookRom() {
         
     	require 'core/init.php';
-    	// Hent valgt dato, bruker, passord, timer valgt
+    	// Hent valgt dato og timenummer
     	$dato = $_POST['dato'];
     	$romNr = $_POST['romnr'];
+
+    	// Kontroller at dato er minst i dag og maks om 30 dager
+    	if ($dato < date("Y-m-d")) {
+    		error("Du kan ikke bestille tilbake i tid");
+    	} else if ($dato > date("Y-m-d", strtotime("+30 days"))) {
+    		error("Du kan ikke bestille mer enn 30 dager fram i tid");
+    	}
+
+    	// Sjekk om bruker har valgt minimum en time å holde av
+        if (isset($_POST['timer']) && !empty($_POST['timer'])) {
+    		$timer = $_POST['timer'];
+    	} else {
+    		error("Du må velge minimum en time som du skal holde av.");
+    	}
         
         // Sjekk om bruker har skrevet et brukernavn
-    	if(isset($_POST['brukernavn']) AND !empty($_POST['brukernavn'])) {
+    	if(isset($_POST['brukernavn']) && !empty($_POST['brukernavn'])) {
     		$brukernavn = $_POST['brukernavn'];
     	} else {
     		error("Du må skrive inn et brukernavn");
     	}
         
         // Sjekk om bruker har skrevet et passord
-    	if (isset($_POST['passord']) AND !empty($_POST['passord'])) {
+    	if (isset($_POST['passord']) && !empty($_POST['passord'])) {
     		$passord = $_POST['passord'];
     		$passord = md5($passord);	// krypter passord med MD5
     	} else {
     		error("Du må skrive inn et passord");
-    	}
-        
-        // Sjekk om bruker har valgt minimum en time å holde av
-        if (isset($_POST['timer']) AND !empty($_POST['timer'])) {
-    		$timer = $_POST['timer'];
-    	} else {
-    		error("Du må velge minimum en time som du skal holde av.");
     	}
         
     	// Hent bruker ID og tilhørende passord fra database
@@ -40,7 +47,7 @@
         
         if (!isset($error)) {
 			// Sjekk om valgt brukernavn eksisterer og passord er riktig før fortsettelse av bestilling
-			if (isset($userID) AND isset($passord) AND $userPW == $passord) {
+			if (isset($userID) && isset($passord) && $userPW == $passord) {
 
 				// Lagre bestilling av rom i databasen på rom, bruker, dato
 				$sql = $database->prepare("INSERT INTO `booking` (`brukerID`, `romNr`, `dato`) VALUES (:userID, :romNr, :dato)");
