6a9df2949c96749a4064efe8c6cbc741553935be
OlavOlsm <OlavOlsm@gmail.com>
2015-03-17 09:41:48 +0100

    Fikset at bruker kan registere bestilling med feil passord

--- nettside/bookRom.php
+++ nettside/bookRom.php
@@ -18,6 +18,7 @@
         // Sjekk om bruker har skrevet et passord
     	if (isset($_POST['passord']) AND !empty($_POST['passord'])) {
     		$passord = $_POST['passord'];
+    		$passord = md5($passord);	// krypter passord med MD5
     	} else {
     		error("Du må skrive inn et passord");
     	}
@@ -36,11 +37,10 @@
 		$user = $sql -> fetch(PDO::FETCH_OBJ);
 		$userID = $user -> brukerID;
 		$userPW = $user -> passord;
-		$userPW = md5($passord);	// krypter passord med MD5
         
         if (!isset($error)) {
 			// Sjekk om valgt brukernavn eksisterer og passord er riktig før fortsettelse av bestilling
-			if (isset($userID) AND isset($passord) AND $userPW = $passord) {
+			if (isset($userID) AND isset($passord) AND $userPW == $passord) {
 
 				// Lagre bestilling av rom i databasen på rom, bruker, dato
 				$sql = $database->prepare("INSERT INTO `booking` (`brukerID`, `romNr`, `dato`) VALUES (:userID, :romNr, :dato)");
@@ -63,12 +63,13 @@
 				// Bestilling er utført.
 			    header("Location: index.php?suksess");
 			}
+
+			// avbryt bestilling dersom brukernavn ikke eksisterer eller passord er feil
+			else {
+				error("Feil brukernavn/passord");
+			}
 		}
         
-		// avbryt bestilling dersom brukernavn ikke eksisterer eller passord er feil
-		else {
-			error("Feil brukernavn/passord");
-		}
 	}
 
 	function error($msg) {
